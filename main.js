398104e6e651566f5b86e06982e3604110926280
kanwar <kdhillon469@gmail.com>
2022-08-24 16:51:46 +1000

    footer  & animation added

--- main.js
+++ main.js
@@ -18,5 +18,59 @@ gsap.from(".animate-hero", {
   duration: 0.6,
   opacity: 0,
   y: -150,
+  stagger: 0.3,
+});
+// gsap.from(".a-serv", {
+//   scrollTrigger: ".a-serv",
+//   duration: 0.2,
+//   opacity: 0,
+//   x: -300,
+//   delay: 0.1,
+// });
+gsap.from(".a-services", {
+  scrollTrigger: ".a-services",
+  duration: 0.4,
+  opacity: 0,
+  x: -300,
   stagger: 0.2,
+  delay: 0.2,
+});
+gsap.from(".a-img", {
+  scrollTrigger: ".a-img",
+  duration: 1,
+  opacity: 0,
+  x: -150,
+  delay: 0.4,
+});
+gsap.from(".a-membership", {
+  scrollTrigger: ".a-membership",
+  duration: 1,
+  opacity: 0,
+  y: -150,
+  stagger: 0.1,
+  delay: 0.6,
+});
+gsap.from(".a-card", {
+  scrollTrigger: ".a-card",
+  duration: 1,
+  opacity: 0,
+  y: -150,
+  stagger: 0.1,
+  delay: 0.6,
+});
+gsap.from(".a-team", {
+  scrollTrigger: ".a-team",
+  duration: 1,
+  opacity: 0,
+  y: -150,
+  stagger: 0.3,
+  delay: 0.6,
+});
+gsap.from(".a-email", {
+  scrollTrigger: ".a-email",
+  duration: 0.8,
+  opacity: 0,
+  y: -150,
+  stagger: 0.25,
+  delay: 0.6,
 });
